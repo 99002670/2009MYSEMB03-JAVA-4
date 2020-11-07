9abfa4efeb4b3dc1110e88def31c368b56cb0d26
99002481 <anubhavreddy.s@ltts.com>
2020-11-07 16:56:25 +0530

    Add transaction functionality to User class

--- BankApplication/src/main/java/bankApplication/User.java
+++ BankApplication/src/main/java/bankApplication/User.java
@@ -26,7 +26,56 @@ public class User {
 		this.accountNumber = accountNumber;
 	}
 	public void transaction() {
-		
+		 System.out.println("Welcome To the Transaction");
+		 int balance=5000, withdraw, deposit;
+		 Scanner s = new Scanner(System.in);
+		 while(true)
+	     {
+		 System.out.println("Press 1: To Deposit an Amount");
+		 System.out.println("Press 2: To Withdraw Money");
+		 System.out.println("Press 3: To Check the Balance remaining");
+		 System.out.println("Press 4: To EXIT and END the Transaction");
+		 System.out.println("..................................................");
+		 System.out.print("Choose the operation you want to perform:");
+		 int n = s.nextInt();
+		 System.out.println("..................................................");
+		 switch(n)
+		 {
+		 case 1:
+		 transactionimplementation t = new transactionimplementation();
+		 Scanner d = new Scanner(System.in);
+		 System.out.println("Enter money to be deposited:-");
+		 deposit = d.nextInt();
+		 balance = t.deposit(1000,deposit);
+		 System.out.println("Your Money has been successfully deposited......Thank you");
+		 System.out.println("Balance Left:" + balance);
+		 System.out.println("..................................................");
+		 System.out.println("..................................................");
+		 break;
+		 
+		 case 2:
+		 transactionimplementation t1 = new transactionimplementation();
+		 Scanner w = new Scanner(System.in);
+		 System.out.print("Enter the amount to be withdrawn:");
+		 withdraw = w.nextInt();
+		 balance = t1.withdraw(balance, withdraw);
+		 System.out.println("Balance Left:" + balance);
+		 System.out.println("..................................................");
+		 System.out.println("..................................................");
+		 break;
+		 
+		 case 3:
+		 transactionimplementation t2 = new transactionimplementation();
+		 balance=(t2.balancecheck(balance));
+		 System.out.println("Balance Left:" + balance);
+		 System.out.println("..................................................");
+		 System.out.println("..................................................");
+		 break;
+		 
+		 case 4:
+		 System.exit(0);
+		}
+		}
 	}
 	public void viewBalance() {
 		
