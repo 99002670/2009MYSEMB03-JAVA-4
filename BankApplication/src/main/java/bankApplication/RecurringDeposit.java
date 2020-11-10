e387067914965886f775e819f20f6766fccf90fb
Milind Mohapatra <milind.mohapatra@ltts.com>
2020-11-10 17:12:50 +0530

    Add getters and setters

--- BankApplication/src/main/java/bankApplication/RecurringDeposit.java
+++ BankApplication/src/main/java/bankApplication/RecurringDeposit.java
@@ -4,7 +4,8 @@ class RecurringDeposit extends Account {
 	protected double amount;
 	final double interestRate = 0.05;
 	protected double tenure;
-	double A, I;
+	double recurringAmount;
+	double recurringInterest;
 
 	RecurringDeposit(String accountNumber, String name, double amount, double durationYears) {
 		super(accountNumber, name);
@@ -12,22 +13,55 @@ class RecurringDeposit extends Account {
 		this.tenure = durationYears;
 	}
 
+	public double getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public double getTenure() {
+		return this.tenure;
+	}
+
+	public void setTenure(double tenure) {
+		this.tenure = tenure;
+	}
+
+	public double getRecurringAmount() {
+		return this.recurringAmount;
+	}
+
+	public void setRecurringAmount(double recurringAmount) {
+		this.recurringAmount = recurringAmount;
+	}
+
+	public double getRecurringInterest() {
+		return this.recurringInterest;
+	}
+
+	public void setRecurringInterest(double recurringInterest) {
+		this.recurringInterest = recurringInterest;
+	}
+
+	public double getInterestRate() {
+		return this.interestRate;
+	}
+
 	void display() {
 		System.out.println("Recurring Deposit Account Details --- ");
-		System.out.println("Number : " + this.accountNumber);
-		System.out.println("Name   : " + this.name);
-		System.out.println("Amount : " + this.amount);
-		System.out.println("Recurring deposit: " + Math.round(A));
-		System.out.println("Interest Earned Amount: " + Math.round(I));
+		System.out.println("Number : " + this.getAccountNumber());
+		System.out.println("Name   : " + this.getName());
+		System.out.println("Amount : " + this.getAmount());
+		System.out.println("Recurring deposit: " + Math.round(this.getRecurringAmount()));
+		System.out.println("Interest Earned Amount: " + Math.round(this.getRecurringInterest()));
 	}
 
 	void calculate() {
-		double temp;
-		this.amount = 15000;
-		this.tenure = 2;
-		temp = 1 + this.interestRate;
-		A = this.amount * Math.pow(temp, this.tenure);
-		I = A - this.amount;
+		double temp = 1 + this.getInterestRate();
+		this.setRecurringAmount(this.getAmount() * Math.pow(temp, this.getTenure()));
+		this.setRecurringInterest(this.getRecurringAmount() - this.getAmount());
 
 	}
 }
